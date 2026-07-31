5411946cdf08cd4b47040565bf6356351c91404a
ManonDamoiseau <m.damoiseau05@hotmail.com>
2026-07-31 16:38:44 +0200

    Add application configuration

---
+++ config/settings.py
@@ -0,0 +1,3 @@
+APPLICATION_NAME = "Automation Supervisior"
+PLC_IP = "127.0.0.1"
+PLC_PORT = 4840
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Automation Supervisor started")
+#print("Automation Supervisor started")
+from config.settings import APPLICATION_NAME, PLC_IP, PLC_PORT
+
+print("===============================")
+print(APPLICATION_NAME)
+print("===============================")
+
+print(f"PLC IP   : {PLC_IP}")
+print(f"PLC PORT : {PLC_PORT}")
+print()
+print("Application started.")
